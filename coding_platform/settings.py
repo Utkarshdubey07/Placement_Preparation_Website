7e01697e94e9cb6d262ae1878ca31ee27b54ca33
Utkarsh Dubey <97867825+Utkarshdubey07@users.noreply.github.com>
2024-07-22 14:05:16 +0530

    Update settings.py

--- coding_platform/settings.py
+++ coding_platform/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-g$35n2#$r)+k6gs7veub%$05vzs3lareu_3t)_a@_0w#&4c#3t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['placement-preparation-website.onrender.com', '127.0.0.1']
 
 
 # Application definition
